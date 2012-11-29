d7db621e62201c06f0aef93d6ac26fc7f18f6c77
Jiaping Zheng <jzheng@cs.umass.edu>
2012-11-29 12:41:51 -0500

    evaluation

--- AuthorCRF/src/main/scala/authorcrf/EvaluatePairs.scala
+++ AuthorCRF/src/main/scala/authorcrf/EvaluatePairs.scala
@@ -1,4 +1,5 @@
 package authorcrf
+import cc.refectorie.user.sameer.util.coref._
 
 object EvaluatePairs {
 
@@ -9,4 +10,14 @@ object EvaluatePairs {
 	def evaluation(publications : Seq[Publication]) {
 		println("Perform cluster evaluation here")
 	}
+
+	def build(pairs : Seq[Pair]): GenericEntityMap[Publication] = {
+		val entMap = new GenericEntityMap[Publication]
+		val pubs = collection.mutable.Set[Publication]()
+		(pubs /: pairs)((s,p) => s ++= Set(p.publication1, p.publication2))
+		pubs.foreach(p => entMap.addMention(p, entMap.numMentions.toLong))
+		pairs.foreach(p => entMap.addCoreferentPair(p.publication1, p.publication2))
+		entMap
+	}
+
 }
